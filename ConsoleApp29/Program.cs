e295edeaa087dd40d195f845f8b9ad35a8829e7a
LikoKuridze <lkuridze@credo.ge>
2024-11-25 01:05:12 +0400

    New

--- ConsoleApp29/Program.cs
+++ ConsoleApp29/Program.cs
@@ -8,14 +8,14 @@ box1.SetValue(5);
 box2.SetValue("string");
 box3.SetValue(true);
 
-Console.WriteLine($"List 1 count: {Utility.j()}");
-Console.WriteLine($"List 2 count: {Utility.j()}");
-Console.WriteLine($"List 3 count: {Utility.j()}");
+Console.WriteLine(box1.GetValue());
+Console.WriteLine(box2.GetValue());
+Console.WriteLine(box3.GetValue());
 
 var values1 = new List<int>() { 1, 2, 3, 4, 5 };
 var values2 = new List<string>() { "hello", "goodbye" };
 var values3 = new List<bool>() { true, true, false, true, false, false };
 
-Utility.j(values1);
-Utility.j(values2);
-Utility.j(values3);
+Console.WriteLine($"List 1 count: {Utility.j(values1)}");
+Console.WriteLine($"List 2 count: {Utility.j(values2)}");
+Console.WriteLine($"List 3 count: {Utility.j(values3)}");
